c3202fbf338b754bf2bed9972db6aa52a3889cb9
Brad Fabiny <bfabiny@starrez.com>
2019-07-17 13:29:13 +1000

    - Removed some unneeded endpoints. - Refactored move calculation into separate class. - Calculate random move

---
+++ TicTacToeServer/Code/MoveGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToeServer.Code
+{
+	public class MoveGenerator
+	{
+		private string[] m_currentState;
+		private const char MyPlayerCharacter = 'O';
+		private const char OtherPlayerCharacter = 'X';
+
+		public MoveGenerator(string currentState)
+		{
+			m_currentState = currentState.Split(',');
+		}
+
+		public string DetermineNextMove()
+		{
+			/* Move Priority:
+			 * 1. Can I win - then win
+			 * 2. Can other player win - then block
+			 * 3. Pick a square at random
+			 */
+
+			string[] nextState = CalculatOWinningMove();
+			if (nextState != null)
+			{
+				return string.Join(',', nextState);
+			}
+
+			nextState = BlockXWinningMove();
+			if (nextState != null)
+			{
+				return string.Join(',', nextState);
+			}
+
+			nextState = MakeRandomMove();
+			return string.Join(',', nextState);
+		}
+
+		private string[] MakeRandomMove()
+		{
+			string[] nextState = m_currentState;
+
+			int index = FindRandomFreeSquare();
+
+			nextState[index] = MyPlayerCharacter.ToString();
+
+			return nextState;
+		}
+
+		private int FindRandomFreeSquare()
+		{
+			int randomSquare = -1;
+
+			List<int> squareIndexes = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
+
+			IEnumerable<int> randomisedIndexes = squareIndexes.OrderBy(x => new Random(DateTime.Now.Millisecond).Next());
+
+			for (int i = 0; i < 9; i++)
+			{
+				if (string.IsNullOrEmpty(m_currentState[randomisedIndexes.ElementAt(i)]))
+				{
+					randomSquare = randomisedIndexes.ElementAt(i);
+					break;
+				}
+			}
+
+			return randomSquare;
+		}
+
+		private string[] CalculatOWinningMove()
+		{
+		}
+
+		private string[] BlockXWinningMove()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- TicTacToeServer/Controllers/MoveController.cs
+++ TicTacToeServer/Controllers/MoveController.cs
@@ -1,79 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using TicTacToeServer.Code;
 
 namespace TicTacToeServer.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
-    {
-        // GET api/move
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MoveController : ControllerBase
+	{
+		// GET api/move
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
 
-        // GET api/move/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+		// GET api/move/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "value";
+		}
 
-        // POST api/move
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+		// POST api/move
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
 
-        // POST api/move/CalculateNextMove
-        [HttpPost]
-        public ActionResult<char[]> CalculateNextMove([FromBody] char[]currentState)
-        {
-            char[] nextState = DetermineNextMove(currentState);
-            return new ActionResult<char[]>(nextState);
-        }
-
-        private char[] DetermineNextMove(char[] currentState)
-        {
-            char[] nextState = currentState;
-
-            if (CanOWin(currentState, out nextState))
-            {
-                return nextState;
-            }
-
-            else if (CanXWin(currentState, out nextState))
-            {
-                return nextState;
-            }
-
-            
-            return currentState;
-        }
-
-        private bool CanOWin(char[] currentState, out char[] nextState)
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool CanXWin(char[] currentState, out char[] nextState)
-        {
-            throw new NotImplementedException();
-        }
-
-        // PUT api/move/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/move/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+		// POST api/move/CalculateNextMove
+		[HttpPost]
+		public ActionResult<string> CalculateNextMove([FromBody] string currentState)
+		{
+			MoveGenerator mover = new MoveGenerator(currentState);
+			string nextState = mover.DetermineNextMove();
+			return new ActionResult<string>(nextState);
+		}
+	}
 }
